3c901a79d64945022e48767c46a1a2ff79987e07
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-02-24 20:43:22 -0800

    Create Vehicle.java

---
+++ Vehicle.java
@@ -0,0 +1,16 @@
+abstract class Vehicle 
+{
+    abstract void move();
+}
+public class Car extends Vehicle 
+{
+    void move() 
+    {
+        System.out.println("Inside the move method of the Car class...");
+    }
+    public static void main(String args[]) 
+    {
+        Vehicle obj = new Car();
+        obj.move();
+    }
+}
