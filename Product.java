40c21e94aa072a832fcb583f3b87eb60133ecb57
Aldo A. Avalos <99375833+aldorithms@users.noreply.github.com>
2023-02-24 20:42:32 -0800

    Create Product.java

---
+++ Product.java
@@ -0,0 +1,21 @@
+public class Product 
+{
+    private String productName;
+    private int productQuantity;
+    public String getProductName() 
+	{
+        return productName;
+    }
+    public void setProductName(String productName) 
+	{
+        this.productName = productName;
+    }
+    public int getProductQuantity() 
+	{
+        return productQuantity;
+    }
+    public void setProductQuantity(int productQuantity) 
+	{
+        this.productQuantity = productQuantity;
+    }
+}
